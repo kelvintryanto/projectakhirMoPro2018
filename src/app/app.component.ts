25a246b7840a9dbe2eae5eaaa66575426cd3c9f6
dikafacriza <triyandika.facriza@student.umn.ac.id>
2018-11-16 15:58:23 +0700

    Dika commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { NewEventPage } from './../pages/new-event/new-event';
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';

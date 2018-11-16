f579a521652288d77d5cb77164e757da52fba428
kelvin.tryanto <kelvin.tryanto@gmail.com>
2018-11-16 23:55:29 +0700

    sign up service KT

--- src/services/SignupService.ts
+++ src/services/SignupService.ts
@@ -4,13 +4,13 @@ import { User } from "../data/userdata.interface";
 export class SignupService{
     
     writeUser(nama: string, email: string, phonenumber: number){
-        const nama,email,phonenumber;
+        // const nama,email,phonenumber;
         
         const keyUser= firebase.database().ref().child('user').push().key
-        const userRef= firebase.database().ref().child('user').child('keyUser');
-        userRef.set(nama:nama);
-        userRef.set(email:email);
-        userRef.set(phonenumber:phonenumber);
+        const userRef= firebase.database().ref().child('user').child(keyUser);
+        // userRef.set(nama:nama);
+        // userRef.set(email:email);
+        // userRef.set(phonenumber:phonenumber);
         
         
     }

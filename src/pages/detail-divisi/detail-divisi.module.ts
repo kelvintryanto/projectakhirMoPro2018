3d9026a858510bc6b8ac02b93ea6732dd3254541
wisan <wisan.maulana@gmail.com>
2018-12-03 18:42:10 +0700

    asd

---
+++ src/pages/detail-divisi/detail-divisi.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { DetailDivisiPage } from './detail-divisi';
+
+@NgModule({
+  declarations: [
+    DetailDivisiPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(DetailDivisiPage),
+  ],
+})
+export class DetailDivisiPageModule {}

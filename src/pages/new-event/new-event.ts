b14f8b351c81a76edf8656b1ffbddf38cf945ed5
Fakhri Faiz <skyvield@gmail.com>
2018-12-12 23:34:10 +0700

    commit ya

--- src/pages/new-event/new-event.ts
+++ src/pages/new-event/new-event.ts
@@ -28,9 +28,39 @@ export class NewEventPage {
   users = firebase.auth().currentUser;
   keyLeader: any;
   user: any[];
+  StartTime:any;
+  EndTime:any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController, public database: AngularFireDatabase) {
+    
+    console.log(this.StartTime);
   }
+  Start(a){
+    console.log("Start")
+    console.log(a);
+    this.StartTime=a;
+  }
+
+  End(b){
+    console.log("End")
+    console.log(b);
+    this.EndTime=b;
+  }
+
+  tes1(time){
+    console.log('jalan StartTime')
+    if(!this.StartTime){
+      this.StartTime=moment().format();
+    }
+  }
+
+  tes2(time){
+    console.log('jalan EndTime')
+    if(!this.EndTime){
+      this.EndTime=moment().format();
+    }
+  }
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad NewEventPage');
@@ -61,8 +91,8 @@ export class NewEventPage {
       eventName: EventName,
       startDate: StartDate,
       endDate: EndDate,
-      startTime: StartTime,
-      endTime: EndTime,
+      startTime: moment(StartTime).format('HH:mm'),
+      endTime: moment(EndTime).format('HH:mm'),
       location: location,
       description: EventDescription,
       leader: keyLeader

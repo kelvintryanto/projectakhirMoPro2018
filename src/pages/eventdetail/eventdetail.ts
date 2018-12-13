75adc912543d492303ba7e82cf47c786011fe64d
kelvin.tryanto <kelvin.tryanto@gmail.com>
2018-12-13 12:23:22 +0700

    testing

--- src/pages/eventdetail/eventdetail.ts
+++ src/pages/eventdetail/eventdetail.ts
@@ -99,6 +99,16 @@ export class EventdetailPage implements OnInit {
 
             this.database.list('/divisi').valueChanges().subscribe(divisi => {
               this.divisi = divisi;
+              console.log(this.divisi)
+              if(this.divisi==null || this.divisi==undefined){
+                firebase.database().ref().child('divisi').child(keyDivisi).set({
+                  keyEvent: keyEvent,
+                  keyDivisi: keyDivisi,
+                  namaDivisi: data.divisi,
+                  progress: ""
+                })
+              }
+              
               for (let idx = 0; idx < divisi.length; idx++) {
                 if (this.divisi[idx].namaDivisi == data.nama) {
                   console.log("nama sama tetap masuk dengan key yang sama")
@@ -126,13 +136,6 @@ export class EventdetailPage implements OnInit {
                   )
                 }
               }
-
-              firebase.database().ref().child('divisi').child(keyDivisi).set({
-                keyEvent: keyEvent,
-                keyDivisi: keyDivisi,
-                namaDivisi: data.divisi,
-                progress: ""
-              })
             })
           }
         }
